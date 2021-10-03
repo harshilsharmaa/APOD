d60e4d6a128b41eac884e7b22d67afc593b562f9
Harshil <harshilsharmaa0@gmail.com>
2021-10-02 20:32:28 -0800

    Some Change

---
+++ script.js
@@ -0,0 +1,63 @@
+fetch('https://api.nasa.gov/planetary/apod?api_key=LqMffyTdTaLX6kpKyN7GZYaih4jvSvUA1W9t9LPg')
+    .then(data => data.json())
+    .then(APOD_Data =>{
+        const APOD_Title = APOD_Data.title;
+        const Title = document.getElementById('title')
+
+        Title.innerHTML = `<h3>${APOD_Title}</h3>`;
+
+        const APOD_date = APOD_Data.date;
+        const date = document.getElementById('date')
+
+        date.innerHTML = APOD_date;
+      
+        const myImage = document.getElementById('image');
+        myImage.innerHTML = `<img src="${ APOD_Data.hdurl}" alt="">`
+        // document.getElementById("image").style.backgroundImage = `url(${ APOD_Data.hdurl})`;
+        // document.getElementById("image").style.backgroundImage = `url(${ APOD_Data.hdurl})`;
+        // document.getElementById("image").style.backgroundPosition = "center";
+        // document.getElementById("image").style.backgroundSize = "cover";
+
+    
+
+
+        const APOD_explanation = APOD_Data.explanation;
+        const explanation = document.getElementById('explanation')
+
+        explanation.innerHTML = APOD_explanation;
+
+        const APOD_copyright = APOD_Data.copyright;
+        const copyright = document.getElementById('copyright')
+
+        copyright.innerHTML = APOD_copyright;
+
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // fetch('https://api.nasa.gov/planetary/apod?api_key=LqMffyTdTaLX6kpKyN7GZYaih4jvSvUA1W9t9LPg')
+    // .then(data => data.json())
+    // .then(APOD_Data =>{
+    //     const APOD_explanation = APOD_Data.explanation;
+    //     const explanation = document.getElementById('explanation')
+
+    //     explanation.innerHTML = APOD_explanation;
+
+    //     const APOD_copyright = APOD_Data.copyright;
+    //     const copyright = document.getElementById('copyright')
+
+    //     copyright.innerHTML = APOD_copyright;
+
+    // })
